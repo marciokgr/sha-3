1f112b9277c2fb3da45c5299f8351bc522313faa
Marcio Kruger - Mirante Tecnologia <mkruger@bancorbras.com.br>
2024-10-09 16:04:31 -0300

    Add project files.

---
+++ sha-3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+if (SHA3_256.IsSupported) //garante que a plataforma suporta
+{
+    var valor = "criptografia SHA3";
+    byte[] data = Convert.FromBase64String(valor);
+
+    byte[] hashsha3 = SHA3_256.HashData(data);
+    string hash = Convert.ToBase64String(hashsha3);
+
+    Console.WriteLine($"valor {valor}");
+    Console.WriteLine($"hash {hash}");
+}
